05b183662e3f751a016d6c66d650d14cf22d4945
Bijoy Islam <bijoy.b19818686@gmail.com>
2026-07-19 18:03:18 +0600

    Add lightbox feature for gallery images
    Implement lightbox functionality for image gallery.

---
+++ art.js
@@ -0,0 +1,32 @@
+const galleryImages = document.querySelectorAll(".gallery img");
+
+const lightbox = document.getElementById("lightbox");
+const lightboxImg = document.getElementById("lightboxImg");
+const closeLightbox = document.getElementById("closeLightbox");
+
+galleryImages.forEach(img=>{
+
+    img.onclick=function(){
+
+        lightbox.style.display="flex";
+        lightboxImg.src=this.src;
+
+    };
+
+});
+
+closeLightbox.onclick=function(){
+
+    lightbox.style.display="none";
+
+};
+
+lightbox.onclick=function(e){
+
+    if(e.target===lightbox){
+
+        lightbox.style.display="none";
+
+    }
+
+};
